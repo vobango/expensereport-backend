477fb5e146fa9e93a20a3b789f885466ecb3aec1
Arvo Rebane <arvo.rebane@gmail.com>
2017-10-06 16:04:58 +0300

    initial Commit

---
+++ src/test/java/com/example/ExpenseReport_Backend/ExpenseReportBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ExpenseReport_Backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpenseReportBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
