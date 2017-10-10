037e6d53a14e766fa3a026dc4070f0b54d385554
kaspar <kaspar.arme@gmail.com>
2017-10-10 14:46:37 +0300

    Add Report controller

---
+++ src/main/java/com/reach_u/expensereport/controller/ReportController.java
@@ -0,0 +1,27 @@
+package com.reach_u.expensereport.controller;
+
+import com.reach_u.expensereport.*;
+import com.reach_u.expensereport.model.Report;
+import com.reach_u.expensereport.service.ReportService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ReportController {
+
+    private ReportService reportService;
+
+    @GetMapping("/reports")
+    public List getReports() {
+        return reportService.list();
+    }
+
+    @GetMapping("/reports/{id}")
+    public Report getReport(@PathVariable("id") int id) {
+        return reportService.get(id);
+    }
+
+}
--- src/main/java/com/reach_u/expensereport/service/ReportService.java
+++ src/main/java/com/reach_u/expensereport/service/ReportService.java
@@ -2,10 +2,7 @@ package com.reach_u.expensereport.service;
 
 import com.reach_u.expensereport.model.*;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * Database "replacement" for expense reports
@@ -15,122 +12,7 @@ import java.util.ListIterator;
  ** Adds methods for using report list / single report data.
  */
 public class ReportService {
-    private static List<Report> reports = new List<Report>() {
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<Report> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] ts) {
-            return null;
-        }
-
-        @Override
-        public boolean add(Report report) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends Report> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int i, Collection<? extends Report> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> collection) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public Report get(int i) {
-            return null;
-        }
-
-        @Override
-        public Report set(int i, Report report) {
-            return null;
-        }
-
-        @Override
-        public void add(int i, Report report) {
-
-        }
-
-        @Override
-        public Report remove(int i) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public ListIterator<Report> listIterator() {
-            return null;
-        }
-
-        @Override
-        public ListIterator<Report> listIterator(int i) {
-            return null;
-        }
-
-        @Override
-        public List<Report> subList(int i, int i1) {
-            return null;
-        }
-    };
+    private static List<Report> reports = new ArrayList<>();
 
     public List list() {
         return reports;
