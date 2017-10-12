655d3f5ff80e2f8e4ae332d215390acad9e2b7db
kaspar <kaspar.arme@gmail.com>
2017-10-12 11:40:43 +0300

    Update Expense document model with JPA and Lombok annotations

--- src/main/java/com/reach_u/expensereport/model/ExpenseDoc.java
+++ src/main/java/com/reach_u/expensereport/model/ExpenseDoc.java
@@ -1,23 +1,48 @@
 package com.reach_u.expensereport.model;
 
-import java.util.Date;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.util.Date;
 /**
  * Model for expense documents (plane tickets, hotel bills etc.)
  *
  ** The expense report will consist of one or more expense documents.
  */
+
+@Entity
+@Table(name="expensedoc")
+@Data
 public class ExpenseDoc {
 
     private int reportId; //which report this document belongs to
+
+    @Id
+    @GeneratedValue
     private int docId; //document's ID in the current report
+
+    @Column(name = "date")
     private Date date;
+
+    @Column(name = "issuer")
     private String issuer;
+
+    @Column(name = "content")
     private String content;
+
+    @Column(name = "field")
     private String field;
+
+    @Column(name = "project")
     private String project;
+
+    @Column(name = "sum")
     private double sum;
+
+    @Column(name = "currency")
     private Currency currency;
+
+    @Column(name = "creditcard")
     private boolean creditCard;
 
     public ExpenseDoc(Date date, String issuer, String content, String field, String project, double sum, Currency currency, boolean creditCard) {
@@ -35,55 +60,7 @@ public class ExpenseDoc {
         this.date = new Date();
     }
 
-    public int getReportId() {
-        return reportId;
-    }
-
-    public void setReportId(int reportId) {
-        this.reportId = reportId;
-    }
-
-    public int getDocId() {
-        return docId;
-    }
-
-    public void setDocId(int docId) {
-        this.docId = docId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public String getIssuer() {
-        return issuer;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public double getSum() {
-        return sum;
-    }
-
     public double getSumEur() {
         return sum*currency.getRate();
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public boolean getCreditCard() {
-        return creditCard;
     }
 }
